158bc21b28599f39110f25fb74846fd8a40f6c14
dgschel <goesc_@hotmail.de>
2024-04-23 16:13:47 +0200

    receive and parse form-data files and log it

--- server/src/functions/upload-files.ts
+++ server/src/functions/upload-files.ts
@@ -1,15 +1,39 @@
 import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/functions";
 
 export async function uploadFiles(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
-    context.log(`Http function processed request for url "${request.url}"`);
+  context.log(`Http function processed request for url "${request.url}"`);
 
-    const name = request.query.get('name') || await request.text() || 'world';
+  try {
+    const formData = await request.formData();
 
-    return { body: `Hello, ${name}!` };
-};
+    if (!formData.has("files")) {
+      return { body: `Specify a key with name 'files'` };
+    }
 
-app.http('upload-files', {
-    methods: ['GET', 'POST'],
-    authLevel: 'anonymous',
-    handler: uploadFiles
+    const formDataValues = formData.getAll("files");
+
+    if (formDataValues.length === 0) {
+      return { body: `Upload atleast one file` };
+    }
+
+    let files: File[] = [];
+
+    for (const file of formDataValues) {
+      if (file instanceof File) {
+        files.push(file);
+      }
+    }
+
+    context.log(files);
+  } catch (error) {
+    return { body: `Specify a body to upload files` };
+  }
+
+  return { body: `Hello, world!` };
+}
+
+app.http("upload-files", {
+  methods: ["POST"],
+  authLevel: "function",
+  handler: uploadFiles,
 });
