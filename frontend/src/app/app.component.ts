9c4b21c346dc43ce851a40fb94b1f185d45979ff
dgschel <goesc_@hotmail.de>
2024-04-27 11:20:50 +0200

    initializes MSAL in root component

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+import { MsalService } from '@azure/msal-angular';
+
 import { HeaderComponent } from './core/components/header/header.component';
 
 @Component({
@@ -7,8 +10,21 @@ import { HeaderComponent } from './core/components/header/header.component';
   standalone: true,
   imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Upload Manager';
+
+  constructor(private authService: MsalService) {}
+
+  ngOnInit(): void {
+    // MSAL v3.x requires initialization of the application object
+    // the method handleRedirectObservable() will initialize and handles the redirect flow
+    this.authService.handleRedirectObservable().subscribe({
+      next: (result) => console.log('Authentication result: ', result),
+      error: (error) => {
+        console.error('Error intializing MSAL: ', error);
+      },
+    });
+  }
 }
