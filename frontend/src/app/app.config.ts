b223f73c430fbac5ff1bb73379425fa07f2e4b01
dgschel <goesc_@hotmail.de>
2024-04-27 11:12:16 +0200

    provide angular a factory with configs to initialize the azure msal service

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,8 +1,53 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { MSAL_INSTANCE, MsalService } from '@azure/msal-angular';
+import {
+  BrowserCacheLocation,
+  IPublicClientApplication,
+  LogLevel,
+  PublicClientApplication,
+} from '@azure/msal-browser';
 
 import { routes } from './app.routes';
+import { environment } from '../environments/environment';
+
+// This is the callback function that will be called by the MSAL logger
+export function loggerCallback(logLevel: LogLevel, message: string) {
+  console.log(message);
+}
+
+export function MSALInstanceFactory(): IPublicClientApplication {
+  return new PublicClientApplication({
+    auth: {
+      clientId: environment.msalConfig.auth.clientId, // This is your client ID from the registered application in Azure AD B2C
+      authority: environment.msalConfig.auth.authority, // This is the authority URL that you specified in the registered application in Azure AD B2C
+      redirectUri: 'https://jwt.ms/', // This is the redirect URL that you specified in the registered application in Azure AD B2C. it needs to match with the configuration
+      knownAuthorities: ['https://uploadmanager.b2clogin.com'], // This is the trusted URL that you specified in Azure AD B2C
+    },
+    cache: {
+      cacheLocation: BrowserCacheLocation.LocalStorage,
+    },
+    system: {
+      allowNativeBroker: false, // Disables WAM Broker
+      loggerOptions: {
+        loggerCallback, // callback function for the MSAL logger
+        logLevel: LogLevel.Info,
+        piiLoggingEnabled: false,
+      },
+    },
+  });
+}
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    importProvidersFrom(BrowserModule),
+    {
+      provide: MSAL_INSTANCE, // This is the token that the MSAL service will use to create the instance
+      useFactory: MSALInstanceFactory, // This is the factory that will create the instance
+    },
+    MsalService, // This is the service that will be used to interact with the MSAL instance
+  ],
 };
