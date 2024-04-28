fd834defea14176c70e42324abf6158271a32ece
dgschel <goesc_@hotmail.de>
2024-04-28 12:34:42 +0200

    add logout method

--- frontend/src/app/core/components/header/header.component.ts
+++ frontend/src/app/core/components/header/header.component.ts
@@ -16,4 +16,9 @@ export class HeaderComponent {
     // Use the redict flow to log in the user
     this.authService.loginRedirect();
   }
+
+  logout() {
+    // Use the redirect flow to log out the user. Clear the Browsercache and redirect to the postLogoutRedirectUri
+    this.authService.logoutRedirect();
+  }
 }
