ba6ac32ca045d2c949787761e0e9c703dc3854d0
dgschel <goesc_@hotmail.de>
2024-04-28 12:30:59 +0200

    add routes for authentication and logout

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from '@angular/router';
 import { UploadComponent } from './upload/upload.component';
 import { HomeComponent } from './home/home.component';
+import { AuthResponseComponent } from './auth/auth-response/auth-response.component';
+import { LogoutComponent } from './logout/logout.component';
 
 export const routes: Routes = [
+  { path: 'auth-response', component: AuthResponseComponent },
   { path: 'upload', component: UploadComponent },
+  { path: 'logout', component: LogoutComponent },
   { path: '', component: HomeComponent },
   { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
