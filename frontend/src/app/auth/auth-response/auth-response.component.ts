69571445bcb7a4a8c353f91cb9b7d37056139dfd
dgschel <goesc_@hotmail.de>
2024-04-28 12:32:56 +0200

    add component after successfull login

---
+++ frontend/src/app/auth/auth-response/auth-response.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-response',
+  standalone: true,
+  imports: [],
+  templateUrl: './auth-response.component.html',
+  styleUrl: './auth-response.component.scss',
+})
+export class AuthResponseComponent {}
