c16e5698c559fd64d768a6ae0ccbe27f42031b33
dgschel <goesc_@hotmail.de>
2024-04-29 09:23:03 +0200

    protect route

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,10 +3,11 @@ import { UploadComponent } from './upload/upload.component';
 import { HomeComponent } from './home/home.component';
 import { AuthResponseComponent } from './auth/auth-response/auth-response.component';
 import { LogoutComponent } from './logout/logout.component';
+import { MsalGuard } from '@azure/msal-angular';
 
 export const routes: Routes = [
   { path: 'auth-response', component: AuthResponseComponent },
-  { path: 'upload', component: UploadComponent },
+  { path: 'upload', component: UploadComponent, canActivate: [MsalGuard] },
   { path: 'logout', component: LogoutComponent },
   { path: '', component: HomeComponent },
   { path: '**', redirectTo: '', pathMatch: 'full' },
