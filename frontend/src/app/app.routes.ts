cc0b7438f8ec0674616d536e3454ccfe68f76484
dgschel <goesc_@hotmail.de>
2024-04-29 09:23:37 +0200

    add login failed component and to route

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { HomeComponent } from './home/home.component';
 import { AuthResponseComponent } from './auth/auth-response/auth-response.component';
 import { LogoutComponent } from './logout/logout.component';
 import { MsalGuard } from '@azure/msal-angular';
+import { LoginFailedComponent } from './login-failed/login-failed.component';
 
 export const routes: Routes = [
   { path: 'auth-response', component: AuthResponseComponent },
   { path: 'upload', component: UploadComponent, canActivate: [MsalGuard] },
   { path: 'logout', component: LogoutComponent },
+  { path: 'login-failed', component: LoginFailedComponent },
   { path: '', component: HomeComponent },
   { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
