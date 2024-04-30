eb0d9940b69e665d70f7f15d87ed0e2838196d06
dgschel <goesc_@hotmail.de>
2024-04-30 20:50:32 +0200

    receive files from child component

--- frontend/src/app/upload/upload.component.ts
+++ frontend/src/app/upload/upload.component.ts
@@ -1,16 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 import { UploadFilesComponent } from './components/upload-files/upload-files.component';
-
+import { FilePropertyComponent } from './components/file-property/file-property.component';
 @Component({
   selector: 'app-upload',
   standalone: true,
-  imports: [UploadFilesComponent],
+  imports: [UploadFilesComponent, FilePropertyComponent],
   templateUrl: './upload.component.html',
   styleUrl: './upload.component.scss',
 })
 export class UploadComponent {
-  onSelectedFiles(files: File[]) {
+  files = signal<File[]>([]);
+
+  handleFileSelection(files: File[]) {
     console.log('Upload Component', files);
+    this.files.set(files);
   }
 }
