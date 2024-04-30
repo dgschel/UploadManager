5ad334a781496958bd1a2aed92dfc5fd44e574ea
dgschel <goesc_@hotmail.de>
2024-04-30 19:23:47 +0200

    add and styled a dropzone component

--- frontend/src/app/upload/components/upload-files/upload-files.component.ts
+++ frontend/src/app/upload/components/upload-files/upload-files.component.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, WritableSignal, signal } from '@angular/core';
+import { NgClass } from '@angular/common';
+
+import { FilePropertyComponent } from '../file-property/file-property.component';
 
 @Component({
   selector: 'app-upload-files',
   standalone: true,
-  imports: [],
+  imports: [NgClass, FilePropertyComponent],
   templateUrl: './upload-files.component.html',
-  styleUrl: './upload-files.component.scss'
+  styleUrl: './upload-files.component.scss',
 })
 export class UploadFilesComponent {
+  files: WritableSignal<Array<File>> = signal<Array<File>>([]);
+  isDragging: WritableSignal<boolean> = signal<boolean>(false);
 
+  onChangedFiles(event: Event) {
+    const target = event.target as HTMLInputElement;
+    if (target.files) {
+      this.files.set(Array.from(target.files));
+    }
+    this.isDragging.set(false);
+
+    console.log('Files', this.files());
+  }
+
+  onDragOver(event: DragEvent) {
+    event.preventDefault();
+    this.isDragging.set(true);
+  }
+
+  onDrop(event: DragEvent) {
+    event.preventDefault();
+    console.log(event.dataTransfer?.files);
+    if (event.dataTransfer?.files) {
+      this.files.set(Array.from(event.dataTransfer.files));
+    }
+    this.isDragging.set(true);
+  }
+
+  onDragLeave(event: DragEvent) {
+    event.preventDefault();
+    this.isDragging.set(false);
+  }
 }
