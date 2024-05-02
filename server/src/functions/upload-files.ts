638a4f9005a55fca3c83a68068901a0ff79bd955
dgschel <goesc_@hotmail.de>
2024-05-02 21:16:02 +0200

    response as json instead of plain text

--- server/src/functions/upload-files.ts
+++ server/src/functions/upload-files.ts
@@ -37,7 +37,7 @@ export async function uploadFiles(request: HttpRequest, context: InvocationConte
     return { body: error.message };
   }
 
-  return { body: `Uploading files to Azure was successful` };
+  return { jsonBody: `Uploading files to Azure was successful` };
 }
 
 app.http("upload-files", {
