83714451bf3664ee5b114aeb3f0af0e539dc9e56
dgschel <goesc_@hotmail.de>
2024-05-04 18:29:10 +0200

    add enpoints and redirecturl

--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -1,10 +1,14 @@
 export const environment = {
-  production: false,
+  production: true,
   msalConfig: {
     auth: {
       clientId: 'e6be80b8-dca2-4ebf-bbbd-2b3aa3f224b1',
       authority:
         'https://uploadmanager.b2clogin.com/uploadmanager.onmicrosoft.com/B2C_1_signupsignin1',
+      redirectUrl: 'http://localhost:4200/auth-response',
     },
+  },
+  endpoints: {
+    fileUpload: 'http://localhost:7071/api/upload-files',
   },
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -5,6 +5,12 @@ export const environment = {
       clientId: 'e6be80b8-dca2-4ebf-bbbd-2b3aa3f224b1',
       authority:
         'https://uploadmanager.b2clogin.com/uploadmanager.onmicrosoft.com/B2C_1_signupsignin1',
+      redirectUrl:
+        'https://stuploadmanagement001.z1.web.core.windows.net/auth-response',
     },
+  },
+  endpoints: {
+    fileUpload:
+      'https://func-uploadmanager-prod-001.azurewebsites.net/api/upload-files?code=CvKS_UXUwx_StdjM-Sm7fwtEg5_635UATnV1sGSTbdWoAzFu9nX35g==',
   },
 };
