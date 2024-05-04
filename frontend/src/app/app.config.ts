8dd36fafb6710734be2b563a2b9388487f221d6f
dgschel <goesc_@hotmail.de>
2024-05-04 23:09:29 +0200

    use env variable for redirectUrl

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -31,7 +31,7 @@ export function MSALInstanceFactory(): IPublicClientApplication {
     auth: {
       clientId: environment.msalConfig.auth.clientId, // This is your client ID from the registered application in Azure AD B2C
       authority: environment.msalConfig.auth.authority, // This is the authority URL that you specified in the registered application in Azure AD B2C
-      redirectUri: '/auth-response', // This is the redirect URL that you specified in the registered application in Azure AD B2C. it needs to match with the configuration
+      redirectUri: environment.msalConfig.auth.redirectUrl, // This is the redirect URL that you specified in the registered application in Azure AD B2C. it needs to match with the configuration
       knownAuthorities: ['https://uploadmanager.b2clogin.com'], // This is the trusted URL that you specified in Azure AD B2C
       navigateToLoginRequestUrl: false, // This is a boolean that specifies whether the application should navigate to the origin URL after login. Set to false to redirect to the redirect URL
       postLogoutRedirectUri: '/logout', // This is the URL that the application will redirect to after logout
