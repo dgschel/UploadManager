4c4dbd942c066cb28f32a0b2471e26796d25b43c
dgschel <goesc_@hotmail.de>
2024-05-08 09:57:18 +0200

    return function with type

--- server/src/utils/validate-jwt-token.ts
+++ server/src/utils/validate-jwt-token.ts
@@ -1,6 +1,7 @@
 import { verify, VerifyOptions } from "azure-ad-verify-token";
+import { JwtPayload } from "jsonwebtoken";
 
-export async function validateToken(token: string): Promise<void> {
+export async function validateToken(token: string): Promise<JwtPayload> {
   const options: VerifyOptions = {
     jwksUri: "https://uploadmanager.b2clogin.com/uploadmanager.onmicrosoft.com/discovery/v2.0/keys?p=B2C_1_signupsignin1", // This is the JWKS URL of the tenant
     issuer: "https://uploadmanager.b2clogin.com/a43b5204-decc-4f2f-bf80-edf6f78ece32/v2.0/", // This is the Issuer URL of the application accessing the tenant
@@ -8,7 +9,8 @@ export async function validateToken(token: string): Promise<void> {
   };
 
   try {
-    await verify(token, options);
+    // Verify the token and return the payload as JwtPayload. Ignore the type string
+    return verify(token, options) as JwtPayload;
   } catch (error) {
     throw new Error(`Failed to validate token`);
   }
