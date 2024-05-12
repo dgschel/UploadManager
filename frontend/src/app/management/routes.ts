771e0682da2815354560be936c4239a528092bfb
dgschel <goesc_@hotmail.de>
2024-05-12 15:52:21 +0200

    add upload route as children

--- frontend/src/app/management/routes.ts
+++ frontend/src/app/management/routes.ts
@@ -1,11 +1,15 @@
 import { Routes } from '@angular/router';
 import { ManagementComponent } from './management.component';
 import { DownloadComponent } from './pages/download/download.component';
+import { UploadComponent } from './pages/upload/upload.component';
 
 export const ManagementRoutes: Routes = [
   {
     path: '',
     component: ManagementComponent,
-    children: [{ path: 'download', component: DownloadComponent }],
+    children: [
+      { path: 'download', component: DownloadComponent },
+      { path: 'upload', component: UploadComponent },
+    ],
   },
 ];
