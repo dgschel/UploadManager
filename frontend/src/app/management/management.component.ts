2c5848e75f63056820ee09e9b1c37f7b6e08b4b5
dgschel <goesc_@hotmail.de>
2024-05-12 16:39:11 +0200

    removed unused import

--- frontend/src/app/management/management.component.ts
+++ frontend/src/app/management/management.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
-import { UploadComponent } from '../upload/upload.component';
 
 @Component({
   selector: 'app-management',
