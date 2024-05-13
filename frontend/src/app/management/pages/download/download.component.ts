f54a6ff77247dc0d8233b5185e5886322b04cfc0
dgschel <goesc_@hotmail.de>
2024-05-13 18:04:14 +0200

    fetch blobs

--- frontend/src/app/management/pages/download/download.component.ts
+++ frontend/src/app/management/pages/download/download.component.ts
@@ -1,20 +1,34 @@
 import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
+
+import { Observable, switchMap } from 'rxjs';
+
+import { DownloadListComponent } from '../../components/download-list/download-list.component';
+import { CustomBlobProperties } from '../../../shared/models/blob';
 import { environment } from '../../../../environments/environment';
 
 @Component({
   selector: 'app-download',
   standalone: true,
-  imports: [HttpClientModule],
+  imports: [HttpClientModule, DownloadListComponent],
   templateUrl: './download.component.html',
   styleUrl: './download.component.scss',
 })
-export class DownloadComponent {
+export class DownloadComponent implements OnInit {
+  blobs = signal<CustomBlobProperties[]>([]);
+
   constructor(private http: HttpClient) {}
 
-  listBlobs() {
-    this.http.get(environment.endpoints.fileDownload).subscribe((data) => {
-      console.log(data);
+  ngOnInit(): void {
+    this.getBlobs().subscribe((data) => {
+      this.blobs.set(data);
+      console.log('Blobs: ', data);
     });
+  }
+
+  private getBlobs(): Observable<CustomBlobProperties[]> {
+    return this.http.get<CustomBlobProperties[]>(
+      environment.endpoints.fileDownload
+    );
   }
 }
