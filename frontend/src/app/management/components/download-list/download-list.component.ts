e7ad8d4f38b6805596206b9fbc33e2f90bd5f071
dgschel <goesc_@hotmail.de>
2024-05-13 20:13:37 +0200

    import ngx-datable module and define input signal

--- frontend/src/app/management/components/download-list/download-list.component.ts
+++ frontend/src/app/management/components/download-list/download-list.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
+import { CustomBlobProperties } from '../../../shared/models/blob';
 
 @Component({
   selector: 'app-download-list',
   standalone: true,
-  imports: [],
+  imports: [NgxDatatableModule],
   templateUrl: './download-list.component.html',
-  styleUrl: './download-list.component.scss'
+  styleUrl: './download-list.component.scss',
 })
 export class DownloadListComponent {
-
+  rows = input.required<CustomBlobProperties[]>();
 }
