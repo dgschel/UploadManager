36d0199efc47c98513ef01b09578b4a6bba11c6a
dgschel <goesc_@hotmail.de>
2024-05-13 18:17:08 +0200

    add error handling, loading with blur

--- frontend/src/app/management/pages/download/download.component.ts
+++ frontend/src/app/management/pages/download/download.component.ts
@@ -1,29 +1,42 @@
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component, OnInit, signal } from '@angular/core';
-
 import { Observable, switchMap } from 'rxjs';
-
 import { DownloadListComponent } from '../../components/download-list/download-list.component';
 import { CustomBlobProperties } from '../../../shared/models/blob';
 import { environment } from '../../../../environments/environment';
+import { LoadingComponent } from '../../../shared/components/loading/loading.component';
 
 @Component({
   selector: 'app-download',
   standalone: true,
-  imports: [HttpClientModule, DownloadListComponent],
+  imports: [HttpClientModule, LoadingComponent, DownloadListComponent],
   templateUrl: './download.component.html',
   styleUrl: './download.component.scss',
 })
 export class DownloadComponent implements OnInit {
   blobs = signal<CustomBlobProperties[]>([]);
+  isLoading = signal<boolean>(false);
 
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
-    this.getBlobs().subscribe((data) => {
-      this.blobs.set(data);
-      console.log('Blobs: ', data);
+    this.isLoading.set(true);
+
+    this.getBlobs().subscribe({
+      next: (data) => this.handleFetchSuccess(data),
+      error: (e) => this.handleFetchError(e),
     });
+  }
+
+  private handleFetchSuccess(data: CustomBlobProperties[]): void {
+    this.isLoading.set(false);
+    this.blobs.set(data);
+    console.log('Blobs: ', data);
+  }
+
+  private handleFetchError(error: any): void {
+    this.isLoading.set(false);
+    console.error('Error: ', error);
   }
 
   private getBlobs(): Observable<CustomBlobProperties[]> {
