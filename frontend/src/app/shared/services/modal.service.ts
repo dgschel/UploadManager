f0dc7477caabb9b7f5d78c299db8bed7ca05fd5a
dgschel <goesc_@hotmail.de>
2024-05-22 14:59:34 +0200

    add logic to create and insert modal into body

--- frontend/src/app/shared/services/modal.service.ts
+++ frontend/src/app/shared/services/modal.service.ts
@@ -1,9 +1,34 @@
-import { Injectable } from '@angular/core';
+import {
+  ApplicationRef,
+  ComponentRef,
+  EnvironmentInjector,
+  Injectable,
+  ViewContainerRef,
+  createComponent,
+} from '@angular/core';
+import { ModalComponent } from '../components/modal/modal.component';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ModalService {
+  componentRef: ComponentRef<ModalComponent> | undefined;
 
-  constructor() { }
+  constructor(
+    private appRef: ApplicationRef,
+    private injector: EnvironmentInjector
+  ) {}
+
+  open(): void {
+    this.componentRef = createComponent(ModalComponent, {
+      environmentInjector: this.injector,
+    });
+
+    document.body.appendChild(this.componentRef.location.nativeElement);
+    this.appRef.attachView(this.componentRef.hostView);
+  }
+
+  close(): void {
+    this.componentRef?.destroy();
+  }
 }
