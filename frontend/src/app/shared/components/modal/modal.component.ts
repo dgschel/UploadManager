fdfcaaa63510aeb0a722391c2d62752e14047252
dgschel <goesc_@hotmail.de>
2024-05-22 09:47:13 +0200

    add markup for modal

---
+++ frontend/src/app/shared/components/modal/modal.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { ModalService } from '../../services/modal.service';
+
+@Component({
+  selector: 'app-modal',
+  standalone: true,
+  imports: [],
+  templateUrl: './modal.component.html',
+  styleUrl: './modal.component.scss',
+})
+export class ModalComponent {
+  constructor(private modalService: ModalService) {}
+
+  open(): void {}
+
+  close(): void {}
+}
