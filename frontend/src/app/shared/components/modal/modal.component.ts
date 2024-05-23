b511cf89565b62ae4172ee390a0078da5b73d87b
dgschel <goesc_@hotmail.de>
2024-05-23 22:06:30 +0200

    clean up component

--- frontend/src/app/shared/components/modal/modal.component.ts
+++ frontend/src/app/shared/components/modal/modal.component.ts
@@ -1,6 +1,4 @@
-import { Component, EventEmitter, TemplateRef, ViewChild } from '@angular/core';
-import { ModalService } from '../../services/modal.service';
-import { Observable, Subject, finalize } from 'rxjs';
+import { Component } from '@angular/core';
 import { LoadingComponent } from '../loading/loading.component';
 
 @Component({
@@ -10,29 +8,4 @@ import { LoadingComponent } from '../loading/loading.component';
   templateUrl: './modal.component.html',
   styleUrl: './modal.component.scss',
 })
-export class ModalComponent {
-  onClose = new Subject<void>();
-
-  close() {
-    console.log('Modal Closing...');
-    this.onClose.next();
-  }
-
-  // approve() {
-  //   // return this.mockHttpCall().pipe(finalize(() => this.modalService.close()));
-  //   return this.mockHttpCall();
-  // }
-
-  // mockHttpCall(): Observable<string> {
-  //   return new Observable<string>((observer) => {
-  //     setTimeout(() => {
-  //       observer.next('Mock response');
-  //       observer.complete();
-  //     }, 3000);
-  //   });
-  // }
-
-  // cancel(): void {
-  //   this.modalService.close();
-  // }
-}
+export class ModalComponent {}
