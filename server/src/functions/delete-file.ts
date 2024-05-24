427f12af35510072bf617fef2023e06836f3c8b3
dgschel <goesc_@hotmail.de>
2024-05-24 09:16:12 +0200

    validate token

--- server/src/functions/delete-file.ts
+++ server/src/functions/delete-file.ts
@@ -1,15 +1,33 @@
 import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/functions";
+import { validateToken } from "../utils/validate-jwt-token.js";
 
 export async function deleteFile(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
-    context.log(`Http function processed request for url "${request.url}"`);
+  context.log(`Http function processed request for url "${request.url}"`);
 
-    const name = request.query.get('name') || await request.text() || 'world';
+  const token = request.headers.get("Authorization");
 
-    return { body: `Hello, ${name}!` };
-};
+  if (!token) {
+    return { jsonBody: { message: "Missing JWT token" } };
+  }
 
-app.http('delete-file', {
-    methods: ['GET', 'POST'],
-    authLevel: 'anonymous',
-    handler: deleteFile
+  const value = token.split(" ")[1];
+
+  try {
+    // Validate the JWT token and retrieve the sub claim
+    // The sub claim is the unique identifier of the user in Azure AD B2C
+    const { sub } = await validateToken(value, process.env["placeholder"]);
+
+    context.log("JWT token is valid");
+
+    return { jsonBody: { message: `placeholder` } };
+  } catch (error) {
+    context.error(`Failed to delete file in Azure: ${error.message}`);
+    return { jsonBody: { message: error.message }, status: 500 };
+  }
+}
+
+app.http("delete-file", {
+  methods: ["GET", "POST"],
+  authLevel: "anonymous",
+  handler: deleteFile,
 });
