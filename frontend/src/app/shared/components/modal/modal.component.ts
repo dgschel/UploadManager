a47e2fcfbc55cb6c4c53eceb24453191c01d50ed
dgschel <goesc_@hotmail.de>
2024-05-29 10:50:56 +0200

    removed unused import

--- frontend/src/app/shared/components/modal/modal.component.ts
+++ frontend/src/app/shared/components/modal/modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, signal } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { LoadingComponent } from '../loading/loading.component';
 
 @Component({
