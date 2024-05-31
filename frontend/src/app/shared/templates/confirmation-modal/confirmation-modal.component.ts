28ff8938c2c7e62c45527147c9b46508927f9520
dgschel <goesc_@hotmail.de>
2024-05-31 11:07:41 +0200

    call http endpoint, handle success and failure

--- frontend/src/app/shared/templates/confirmation-modal/confirmation-modal.component.ts
+++ frontend/src/app/shared/templates/confirmation-modal/confirmation-modal.component.ts
@@ -1,16 +1,19 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import {
   Component,
   EventEmitter,
+  Input,
   Output,
   TemplateRef,
   ViewChild,
 } from '@angular/core';
-import { Subject, Observable, of, delay } from 'rxjs';
+import { Subject, Observable, throwError, catchError, tap } from 'rxjs';
+import { environment } from '../../../../environments/environment';
 
 @Component({
   selector: 'app-confirmation-modal',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule],
   templateUrl: './confirmation-modal.component.html',
   styleUrl: './confirmation-modal.component.scss',
 })
@@ -18,23 +21,36 @@ export class ConfirmationModalComponent {
   @ViewChild('container', { static: true, read: TemplateRef })
   containerTemplate: TemplateRef<any> | undefined;
 
+  @Input() fileName: string = '';
+
   private startSubject = new Subject<boolean>();
   start$: Observable<boolean> = this.startSubject.asObservable();
 
   private submitSubject = new Subject<string>();
   submit$: Observable<string> = this.submitSubject.asObservable();
 
+  private errorSubject = new Subject<string>();
+  error$: Observable<string> = this.errorSubject.asObservable();
+
   private closeEvent = new EventEmitter<void>();
   @Output() onClose: EventEmitter<void> = this.closeEvent;
 
-  protected submit(): void {
-    this.startSubject.next(true);
+  constructor(private http: HttpClient) {}
 
-    of('Mock HTTP request complete!')
-      .pipe(delay(5000))
-      .subscribe((message) => {
-        this.submitSubject.next(message);
-        this.startSubject.next(false);
+  protected submit(): void {
+    this.http
+      .delete(`${environment.endpoints.fileDelete}/${this.fileName}`)
+      .pipe(
+        tap(() => this.startSubject.next(true)),
+        catchError((err) => throwError(() => err)) // Pass the error to the next observer
+      )
+      .subscribe({
+        next: (res) => console.log('File deleted!', res),
+        error: (err) => {
+          console.error('Error deleting file', err.message);
+          this.errorSubject.next(err.message);
+        },
+        complete: () => this.startSubject.next(false),
       });
   }
 
